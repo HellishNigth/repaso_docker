570bb82f4b5a40e49d49b5c7253083a7d2212ebc
Ignacio <kritka200@gmail.com>
2021-06-21 20:27:43 -0400

    Avance hasta crear el master.blade.php

---
+++ web/repaso_docker/resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends("layouts.master")
+
+@section("contenido")
+        <h1>hola soy home</h1>
+@endsection
---
+++ web/repaso_docker/resources/views/ver_consolas.blade.php
@@ -0,0 +1,5 @@
+@extends("layouts.master")
+
+@section("contenido")
+        <h1>hoal soy ver consolas</h1>
+@endsection
