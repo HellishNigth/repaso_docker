64e9ed6f16da5a6d598c966bfe129dbb769ce16c
Ignacio <kritka200@gmail.com>
2021-06-23 18:51:12 -0400

    avance hasta crear la carpeta axios

--- web/repaso_docker/resources/views/ver_consolas.blade.php
+++ web/repaso_docker/resources/views/ver_consolas.blade.php
@@ -1,5 +1,21 @@
 @extends("layouts.master")
 
 @section("contenido")
-        <h1>hoal soy ver consolas</h1>
+        <div class="row mt-5">
+                <div class="col-12 col-mb-12 col-lg-6 mx-auto">
+                        <table class="table table-hover table-bordered table-striped table-responsive">
+                                <thead class="bg-info">
+                                        <tr>
+                                                <td>Nombre</td>
+                                                <td>Marca</td>
+                                                <td>Año de lanzamiento</td>
+                                                <td>Acciones</td>
+                                        </tr>
+                                </thead>
+                                <tbody id="tbody-consola">
+
+                                </tbody>
+                        </table>
+                </div>
+        </div>
 @endsection
