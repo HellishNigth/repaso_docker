d899172df1f724ca8c59b44058c36d0342c0dc3e
Ignacio <kritka200@gmail.com>
2021-07-22 20:26:24 -0400

    Agregar controlador de Juegos y Modelo de Juegos

--- web/repaso_docker/routes/api.php
+++ web/repaso_docker/routes/api.php
@@ -3,16 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ConsolasController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\JuegosController;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
@@ -25,3 +16,9 @@ Route::get("consolas/filtrar", [ConsolasController::class, "filtrarConsolas"]);
 
 Route::post("consolas/post", [ConsolasController::class, "crearConsola"]);
 Route::post("consolas/delete", [ConsolasController::class,"eliminarConsola"]);
+
+
+Route::get("juegos/get", [JuegosController::class, "getJuegos"]);
+Route::get("juegos/getByConsola", [JuegosController::class, "getJuegosByController"]);
+Route::post("juegos/post", [JuegosController::class, "save"]);
+Route::post("juegos/delete", [JuegosController::class, "remove"]);
